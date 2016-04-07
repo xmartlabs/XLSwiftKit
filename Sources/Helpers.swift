03324cd1af21267af9c77a81728e6742ed38be5f
Martin Barreto <martin@xmartlabs.com>
2016-04-06 23:01:08 -0300

    added some helpers

---
+++ Sources/Helpers.swift
@@ -0,0 +1,35 @@
+//  Helpers.swift
+//  XLSwiftKit ( https://github.com/xmartlabs/XLSwiftKit )
+//
+//  Copyright (c) 2016 Xmartlabs SRL ( http://xmartlabs.com )
+//
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+import Foundation
+
+func JSONStringify(value: AnyObject, prettyPrinted: Bool = true) -> String {
+    let options: NSJSONWritingOptions = prettyPrinted ? .PrettyPrinted : []
+    if NSJSONSerialization.isValidJSONObject(value) {
+        if let data = try? NSJSONSerialization.dataWithJSONObject(value, options: options), let string = NSString(data: data, encoding: NSUTF8StringEncoding) {
+            return string as String
+        }
+    }
+    return ""
+}
